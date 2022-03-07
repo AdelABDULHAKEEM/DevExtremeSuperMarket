be2ea4c39f0fad5b3e6b4ac906341423afb6122b
adel abd ulhakeem <adel20142658@gmail.com>
2022-03-07 21:10:35 +0200

    first commit

---
+++ Models/SuperMarketDB.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DevExtremeSuperMarket.Models;
+using Microsoft.IdentityModel.Protocols;
+
+namespace DevExtremeSuperMarket.Models
+{
+    public partial class SuperMarketDB : DbContext
+    {
+        public SuperMarketDB()
+        {
+        }
+        public SuperMarketDB(DbContextOptions<SuperMarketDB> options)
+        
+            : base(options)
+        { 
+        }
+        
+        public virtual DbSet<SampleOrder> ordres { get; set; }
+        
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using DevExtremeSuperMarket.Models;
+
+
+namespace DevExtremeSuperMarket
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddDbContext<SuperMarketDB>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("SuperMarketDatabase")));
+            // Add framework services.
+            services
+                .AddRazorPages()
+                .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
